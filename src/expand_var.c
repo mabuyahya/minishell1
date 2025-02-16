4a982fa422b767ac940e1daaf3fd61826bb8c5d0
mabuyahya <mhmad20shehadeh@gmail.com>
2025-02-16 17:08:26 +0300

    finish repo work

--- src/expand_var.c
+++ src/expand_var.c
@@ -1,27 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   expand_var.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mabuyahy <mabuyahy@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/16 13:29:14 by mabuyahy          #+#    #+#             */
-/*   Updated: 2025/02/16 13:29:59 by mabuyahy         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
-static void	expand_variable_util(t_expand_read_2 *var,
-	char *line, int prefix_len)
-{
-	ft_strncpy(var->expanded, line, prefix_len);
-	var->expanded[prefix_len] = '\0';
-	ft_strcat(var->expanded, var->env_value);
-	free(var->env_value);
-	strcat(var->expanded, var->key_eend);
-	free(line);
-}
+extern int g_status;
+
+// static void	expand_variable_util(t_expand_read_2 *var,
+// 	char *line, int prefix_len)
+// {
+// 	ft_strncpy(var->expanded, line, prefix_len);
+// 	var->expanded[prefix_len] = '\0';
+// 	ft_strcat(var->expanded, var->env_value);
+// 	free(var->env_value);
+// 	strcat(var->expanded, var->key_eend);
+// 	free(line);
+// }
 
 static char *get_env_value(char *key, t_prompt *main)
 {
@@ -39,38 +29,73 @@ static char *get_env_value(char *key, t_prompt *main)
 //         free(*env_value);
 // }
 
-static char *expand_unquoted_variable(t_expand_read_2 *var)
+static char *expand_variable_standard(char *line, char *key_start, t_expand_read_2 *var)
 {
-    char **words;
-    char *temp;
-    int i;
+	size_t prefix_len;
+	size_t env_len;
+	size_t suffix_len;
+	size_t new_len;
+	char *new_line;
 
-    words = ft_split(var->env_value, ' ');
-    free(var->env_value);
-    if (!words)
-        return (NULL);
-    var->env_value = ft_strdup("");
-    i = -1;
-    while (words[++i])
-    {
-        temp = ft_strjoin("\"", words[i]);
-        free(words[i]);
-        words[i] = ft_strjoin(temp, "\"");
-        free(temp);
-        temp = var->env_value;
-        var->env_value = ft_strjoin(temp, words[i]);
-        free(temp);
-        free(words[i]);
-        if (words[i + 1])
-        {
-            temp = var->env_value;
-            var->env_value = ft_strjoin(temp, " ");
-            free(temp);
-        }
-    }
-    free(words);
-    return (var->env_value);
+	prefix_len = key_start - line - 1; // Adjust to remove $
+	env_len = ft_strlen(var->env_value);
+	suffix_len = ft_strlen(var->key_eend);
+	new_len = prefix_len + env_len + suffix_len; // No extra quotes
+
+	new_line = ft_calloc(new_len + 1, 1);
+	if (!new_line)
+	{
+		free(var->env_value);
+		return (line);
+	}
+
+	// Copy prefix (before $)
+	ft_strncpy(new_line, line, prefix_len);
+	new_line[prefix_len] = '\0';
+
+	// Append expanded value
+	strcat(new_line, var->env_value);
+
+	// Append suffix (remaining part of line after the variable)
+	strcat(new_line, var->key_eend);
+
+	free(var->env_value);
+	free(line);
+	return (new_line);
 }
+
+
+
+static char *expand_unquoted_variable(t_expand_read_2 *var, char *line)
+{
+	char **words;
+	char *temp;
+	int i;
+
+	words = ft_split(var->env_value, ' ');
+	free(var->env_value);
+	if (!words)
+		return (NULL);
+	var->env_value = ft_strdup("");
+	i = -1;
+	while (words[++i])
+	{
+		temp = var->env_value;
+		var->env_value = ft_strjoin(temp, words[i]);
+		free(temp);
+		free(words[i]);
+		if (words[i + 1]) // Add space only if not the last word
+		{
+			temp = var->env_value;
+			var->env_value = ft_strjoin(temp, " ");
+			free(temp);
+		}
+	}
+	free(words);
+	return expand_variable_standard(line, var->key_eend, var);
+}
+
+
 
 static char *expand_quoted_variable(t_expand_read_2 *var, char *line, size_t new_len, size_t prefix_len)
 {
@@ -80,62 +105,54 @@ static char *expand_quoted_variable(t_expand_read_2 *var, char *line, size_t new
         free(var->env_value);
         return (line);
     }
+
     ft_strncpy(var->expanded, line, prefix_len);
     var->expanded[prefix_len] = '\0';
-    ft_strcat(var->expanded, "'");
-    ft_strcat(var->expanded, var->env_value);
-    ft_strcat(var->expanded, "'");
+
+    // Directly append env_value without extra quotes
+    strcat(var->expanded, var->env_value);
+    strcat(var->expanded, var->key_eend);
+
+    free(var->env_value);
+    free(line);
     return (var->expanded);
 }
 
-static char *expand_variable_standard(char *line, char *key_start, t_expand_read_2 *var)
-{
-    size_t new_len = ft_strlen(line) - var->len + ft_strlen(var->env_value);
-    size_t prefix_len = key_start - line - 1;
-
-    var->expanded = ft_calloc(new_len + 1, 1);
-    if (!var->expanded)
-    {
-        free(var->env_value);
-        return (line);
-    }
-    expand_variable_util(var, line, prefix_len);
-    return (var->expanded);
-}
 
 static char *expand_variable(char *line, char *key_start, t_prompt *main, int quoted)
 {
-    t_expand_read_2 var;
-    size_t new_len;
-    size_t prefix_len;
+	t_expand_read_2 var;
+	size_t prefix_len;
 
-    var.key_eend = key_start;
-    while (*var.key_eend && (ft_isalnum(*var.key_eend) || *var.key_eend == '?'))
-        var.key_eend++;
-    var.len = var.key_eend - key_start;
-    var.key = ft_strndup(key_start, var.len);
-    if (!var.key)
-        return (line);
+	var.key_eend = key_start;
+	while (*var.key_eend && (ft_isalnum(*var.key_eend) || *var.key_eend == '?'))
+		var.key_eend++;
+	var.len = var.key_eend - key_start;
+	var.key = ft_strndup(key_start, var.len);
+	if (!var.key)
+		return (line);
 
-    var.env_value = get_env_value(var.key, main);
-    free(var.key);
+	var.env_value = get_env_value(var.key, main);
+	free(var.key);
 
-    // Handle unquoted expansion case
-    if (!quoted)
-    {
-        return expand_unquoted_variable(&var);
-    }
+	prefix_len = key_start - line - 1;
 
-    // Special logic for single-quoted expansion: Keep single quotes
-    if (quoted && line[0] == '\'' && line[strlen(line) - 1] == '\'')
-    {
-        new_len = ft_strlen(line) - var.len + ft_strlen(var.env_value) + 2;
-        prefix_len = key_start - line - 1;
-        return expand_quoted_variable(&var, line, new_len, prefix_len);
-    }
+	// Handle unquoted expansion case
+	if (!quoted)
+	{
+		return expand_unquoted_variable(&var, line);
+	}
 
-    return expand_variable_standard(line, key_start, &var);
+	// Special logic for single-quoted expansion: Keep single quotes
+	if (quoted && line[0] == '\'' && line[strlen(line) - 1] == '\'')
+	{
+		size_t new_len = ft_strlen(line) - var.len + ft_strlen(var.env_value) + 2;
+		return expand_quoted_variable(&var, line, new_len, prefix_len);
+	}
+
+	return expand_variable_standard(line, key_start, &var);
 }
+
 
 
 static void	init_expand_read(t_expand_read *var, char *line)
